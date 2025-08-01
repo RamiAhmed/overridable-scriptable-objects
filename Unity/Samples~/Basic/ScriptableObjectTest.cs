dcc311ddd32903286c5e840f0acc08c2a0e0be5e
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2025-08-02 00:41:37 +0200

    Samples folder rename to ignore in Unity package

---
+++ Unity/Samples~/Basic/ScriptableObjectTest.cs
@@ -0,0 +1,13 @@
+﻿using OverridableScriptableObjects.Runtime;
+using UnityEngine;
+
+namespace OverridableScriptableObjects.Samples.Basic
+{
+    [CreateAssetMenu(fileName = "ScriptableObjectTest", menuName = "OverridableScriptableObjects/ScriptableObjectTest")]
+    public class ScriptableObjectTest : OverridableScriptableObject
+    {
+        public string TestString = "ABC";
+        public float TestFloat = 123.456f;
+        public int TestInt = 789;
+    }
+}
